cf1aae55beaa7eda303f425d79b6c2c93d11f1c2
Shawn Jung <shawn.jung@me.com>
2021-02-21 18:40:53 -0800

    frontend prototype

--- popup.js
+++ popup.js
@@ -53,7 +53,7 @@ $(function() {
                             output[key] = targetElement.textContent
                         else {
                             console.warn(`cannot find element by id: ${map[key]}`)
-                            output[key] = 'Unknown'   
+                            output[key] = null
                         }
                         break;
                     case "object":
@@ -67,7 +67,7 @@ $(function() {
         return output
     }
     
-    let propertyIdMap = {
+    const propertyIdMap = {
         address: "mainaddresstitle",
         latest: {
             totalValue: "lblTotalAssessedValue",
@@ -78,20 +78,136 @@ $(function() {
             totalValue: "lblPreviousAssessedValue",
             landValue: "lblPreviousAssessedLand",
             buildingValue: "lblPreviousAssessedBuilding"
+        },
+        extraInformation: {
+            yearBuilt: "lblYearBuilt",
+            description: "lblDescription",
+            bedrooms: "lblBedrooms",
+            bathrooms: "lblBathRooms",
+            carports: "lblCarPorts",
+            garages: "lblGarages",
+            landSize: "lblLandSize",
+            firstFloorArea: "lblFirstFloorArea",
+            secondFloorArea: "lblSecondFloorArea",
+            basementFinishArea: "lblBasementFinishArea",
+            strataArea: "lblStrataTotalArea",
+            buildingStoreys: "lblStoriesBuilding",
+            numberOfApartmentUnits: "lblNumberUnitApartment",
         }
     }
+    
+    const extraInformationLabels = {
+        address: "Address",
+        yearBuilt: "Year Built",
+        description: "Description",
+        landSize: "Land Size",
+        strataArea: "Strata sqft",
+        numberOfApartmentUnits: "No.of Units",
+        firstFloorArea: "1st Floor sqft",
+        secondFloorArea: "2nd Floor sqft",
+        basementFinishArea: "Basement sqft",
+        buildingStoreys: "Storeys",
+        bedrooms: "Bedrooms",
+        bathrooms: "Bathrooms",
+        carports: "Carports",
+        garages: "Garages",
+    }
 
-    let insertInfo = (assessment) => {
+    const convertToInt = (string) => {
+        return parseInt(string.replace(/[^0-9.]/g, ''))
+    }
+    
+    const getChanges = (current, previous) => {
+        if (!current || !previous) {
+            return null
+        }
+        
+        let style, prefix
+        const value = convertToInt(current) / convertToInt(previous) * 100 - 100
+        
+        if (value === 0) {
+            return null
+        }
+        
+        if (value > 0) {
+            style = 'positive',
+            prefix = '+'
+        } else if (value < 0) {
+            style = 'negative'
+            prefix = ''
+        } 
+        return { style, value: `${prefix}${value.toFixed(1)}%` }
+    }
+    
+
+    const insertInfo = (assessment) => {
         if (assessment) {
-            $('#address').text(assessment.address)
-            $('.latest.total.value').text(assessment.latest.totalValue)
-            $('.latest.land.value').text(assessment.latest.landValue)
-            $('.latest.building.value').text(assessment.latest.buildingValue)
-            $('.previous.total.value').text(assessment.previous.totalValue)
-            $('.previous.land.value').text(assessment.previous.landValue)
-            $('.previous.building.value').text(assessment.previous.buildingValue)
-            $('#assessmentLink').click(function() {
-                chrome.tabs.create({url: assessment.link})
+            $('body').removeClass('loading')
+            
+            let hasDetailedValuation = false
+            const { latest, previous, extraInformation, origAddress, link } = assessment
+            const totalChanges = getChanges(latest.totalValue, previous.totalValue)
+            const landChanges = getChanges(latest.landValue, previous.landValue)
+            const buildingChanges = getChanges(latest.buildingValue, previous.buildingValue)
+            
+            $('.total.valuation .value').text(latest.totalValue)
+            $('.total.valuation .previous-value .amount').text(previous.totalValue)
+            
+            if (totalChanges) {
+                $('.total.valuation .changes')
+                    .addClass(totalChanges.style)
+                    .text(totalChanges.value)
+            }
+            
+            if(latest.landValue) {
+                $('.land.valuation .value').text(latest.landValue)    
+                $('.land.valuation .previous-value .amount').text(previous.landValue)
+                hasDetailedValuation = true
+                
+                if (landChanges) {
+                    $('.land.valuation .changes')
+                        .addClass(landChanges.style)
+                        .text(landChanges.value)
+                }
+            } else {
+                $('.land.valuation').addClass('unknown')
+            }
+            
+            if(latest.buildingValue) {
+                $('.building.valuation .value').text(latest.buildingValue)
+                $('.building.valuation .previous-value .amount').text(previous.buildingValue)
+                hasDetailedValuation = true
+                
+                if (buildingChanges) {
+                    $('.building.valuation .changes')
+                        .addClass(buildingChanges.style)
+                        .text(buildingChanges.value)
+                }
+            } else {
+                $('.building.valuation').addClass('unknown')
+            }
+            
+            if (hasDetailedValuation) {
+                $('.valuations').addClass('has-detailed-valuation')
+            }
+            
+            if(origAddress) {
+                $('.original-address.value').text(origAddress)
+            }
+
+            const $extraItemList = $(".extra-item-list")
+            _(extraInformationLabels).each((label, key) => {
+                const extraValue = s(extraInformation[key]).trim().value()
+                if (extraValue) {
+                    const $extraItem = $("<div class='extra-item'>")
+                    $extraItem.append($("<div class='label'>").text(label))
+                    $extraItem.append($("<div class='value'>").text(extraValue))
+                    $extraItemList.append($extraItem)
+                }
+            })
+            
+            $('.view-on-bc-assessment.btn').click(function() {
+                chrome.tabs.create({url: link})
             })
         }
     }  
