fc280a8dec30b4518af4dd8fe9d4d17baa304aef
Jenny Song <jenny.jieun.song@gmail.com>
2021-03-05 02:04:32 -0800

    clean up background.js

--- background.js
+++ background.js
@@ -4,43 +4,50 @@ var ZOLO_REGEX = new RegExp(/https:\/\/www.zolo.ca\/.+\/.+/)
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.title) {
-        openScript(tab.url, changeInfo.title)
+        getHomeDetails(tab.url, changeInfo.title)
     }
 })
  
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab){
-        openScript(tab.url, tab.title)
+        getHomeDetails(tab.url, tab.title)
     })
 })
 
-openScript = (url, address) => {
+var getHomeDetails = (url, address) => {
+    var site = url.match(REDFIN_REGEX)? 'redfin' :
+        url.match(REALTOR_REGEX)? 'realtor' :
+        url.match(ZOLO_REGEX)? 'zolo' : null
+    
+    if (site) {
+        getHomeAddress(site, address)
+        getHomePrice(site)
+    }
+}
+
+var getHomeAddress = (site, address) => {
     // Redfin
-    if (url.match(REDFIN_REGEX)) { 
+    if (site === 'redfin') { 
         var a = address.split(', BC')[0]
         var i = a.indexOf('#')
         if (i > 0) {
             var j = a.indexOf(',') 
             a = a.substring(i+1,j) + '-' + a.substring(0,i-1) + a.substring(j+1)
         }
-        chrome.storage.sync.set({'bcre-address': a})
-        getSiteDetail('redfin')
     }
     // Realtor.ca
-    if (url.match(REALTOR_REGEX)) {
+    if (site === 'realtor') {
         var a = address.split('For sale: ')[1].split(', British Columbia')[0]
-        chrome.storage.sync.set({'bcre-address': a})
-        getSiteDetail('realtor')
     }
-    // Todo: Zolo
-    if (url.match(ZOLO_REGEX)) {
+    // Zolo
+    if (site === 'zolo') {
         var a = address.split(' — For Sale')[0]
-        chrome.storage.sync.set({'bcre-address': a})
-        getSiteDetail('zolo')
     }
+    chrome.storage.sync.set({'bcre-address': a})
 }
 
-getSiteDetail = (site) => {
+
+var getHomePrice = (site) => {
     chrome.tabs.executeScript({
         code: `var site = "${site}"`
     }, () => {
